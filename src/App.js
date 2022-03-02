8d554827752243844842990d66e544d6b68c1379
jarol <jaancastano@utp.edu.co>
2022-03-02 16:32:15 -0500

    agregar componente de Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
      <span className='fondoNegro'>Hola mundo</span>
      <button type="button" className="btn btn-primary">Primary</button>
+     <Card/>
     </div>
   );
 }
